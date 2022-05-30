292fada773da5e724ac330a812b7c18e11e7d0e5
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2022-05-31 01:10:04 +0530

    Time: 40 ms (99.69%), Space: 33.6 MB (79.91%) - LeetHub

---
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            int ind=abs(nums[i])-1;
+            if(nums[ind]<0){
+                ans.push_back(abs(nums[i]));
+            }
+            else{
+                nums[ind]=-nums[ind];
+            }
+        }
+        return ans;
+    }
+};
