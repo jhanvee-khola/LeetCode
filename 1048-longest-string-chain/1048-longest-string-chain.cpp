86e59b78d18ec6f565d130ba97354fb02cc5ab2b
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2022-06-16 01:53:09 +0530

    Time: 111 ms (71.53%), Space: 27.2 MB (21.11%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:  
+    static bool compare(string s,string t){
+        return s.length()<t.length();
+    }
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(),words.end(),compare);
+        unordered_map<string,int> dp;
+
+        int ans=1; 
+        for(auto word:words) {
+            dp[word]=1; 
+            for(int i=0;i<word.length();i++) {
+                string pred= word.substr(0,i) + word.substr(i+1); 
+                if(dp.find(pred)!=dp.end()) {
+                    dp[word]=dp[pred]+1; 
+                    ans=max(ans, dp[word]);
+                }
+            }
+        }
+        return ans;  
+    }
+};
