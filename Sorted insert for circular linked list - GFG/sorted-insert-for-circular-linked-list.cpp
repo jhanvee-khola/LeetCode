43cd755df0e4dafc8e30d0106d6c6a4d318bc0ad
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2022-07-02 23:29:22 +0530

    Added solution - LeetHub

---
+++ Sorted insert for circular linked list - GFG/sorted-insert-for-circular-linked-list.cpp
@@ -0,0 +1,135 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+/* structure for a Node */
+struct Node
+{
+  int data;
+  struct Node *next;
+  
+  Node(int x){
+      data = x;
+      next = NULL;
+  }
+  
+};
+
+
+ // } Driver Code Ends
+/* structure for a node */
+/*
+struct Node
+{
+  int data;
+  struct Node *next;
+  
+  Node(int x){
+      data = x;
+      next = NULL;
+  }
+  
+};
+ */
+class Solution{
+    public:
+    Node *sortedInsert(Node* head, int x){
+        Node* key=(Node*)malloc(sizeof(Node));
+        key->data=x;
+        Node *tail=(Node*)malloc(sizeof(Node));
+        tail=head;
+        while(tail->next!=head){
+            tail=tail->next;
+        }
+        if(head->data>x){
+            key->next=head;
+            tail->next=key;
+            return key;
+        }
+        Node *temp=head;
+        while(1){
+            if(temp->next->data>=x){
+                Node* temp1=(Node*)malloc(sizeof(Node));
+                temp1=temp->next;
+                temp->next=key;
+                key->next=temp1;
+                return head;
+            }
+            else{
+                if(temp==tail){
+                    temp->next=key;
+                    key->next=head;
+                    return head;
+                }
+                temp=temp->next;
+            }
+        }
+    }
+};
+
+// { Driver Code Starts.
+
+/* Function to print Nodes in a given linked list */
+void printList(struct Node *start)
+{
+  struct Node *temp;
+
+  if(start != NULL)
+  {
+    temp = start;
+    do { printf("%d ", temp->data);
+      temp = temp->next;
+    } while(temp != start);
+    printf("\n");
+  }
+}
+/* Driver program to test above functions */
+int main()
+{
+int t,n,x;
+scanf("%d",&t);
+int arr;
+  while(t--){
+  scanf("%d",&n);
+  int list_size, i;
+  
+  /* start with empty linked list */
+  struct Node *start = NULL;
+  struct Node *temp,*r;
+  
+  
+    /* Create linked list from the array arr[].
+    Created linked list will be 1->2->11->56->12 */
+    if(n>0){
+      scanf("%d",&arr);
+    
+    temp = new Node(arr);
+    start=temp;
+    r=start;
+    }
+    for (i = 0; i<n-1; i++)
+    {
+        scanf("%d",&arr);
+        temp = new Node(arr);
+        r->next=temp;
+        r=r->next;
+    }
+    
+    if(n>0)
+    temp->next=start;
+ 
+    scanf("%d",&x);
+    Solution ob;
+    start = ob.sortedInsert(start,x);
+    printList(start);
+    r=start;
+    while(r!=start->next)
+    {
+
+      temp=start;
+      start=start->next;
+      free(temp);
+    }
+    free(start);
+}
+  return 0;
+}
+  // } Driver Code Ends
