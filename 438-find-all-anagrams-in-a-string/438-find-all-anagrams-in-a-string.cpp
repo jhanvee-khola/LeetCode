129c4e6d1945deee2b30d007bf62f9f9ccf22982
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2022-07-14 03:53:58 +0530

    Time: 34 ms (35.16%), Space: 8.6 MB (74.44%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
+        int n=s.length(),m=p.length();
+        unordered_map<char,int> freq;
+        for(int i=0;i<m;i++){
+            freq[p[i]]++;
+        }
         vector<int> ans;
-        if(s.length()<p.length()){
-            return ans;
-        }
-        int start=0,end=p.length()-1;
-        unordered_map<char,int> mp,ms;
-        for(int i=0;i<p.length();i++){
-            mp[p[i]]++;
-            ms[s[i]]++;
-        }
-        int m=p.length();
-        for(int i=m;i<s.length();i++){
-            if(ms==mp){
-                ans.push_back(i-m);
+        int start=0,match=0;
+        for(int end=0;end<n;end++){
+            if(freq.find(s[end])!=freq.end()){
+                freq[s[end]]--;
+                if(freq[s[end]]==0){
+                    match++;
+                }
             }
-            ms[s[i]]++;
-            ms[s[i-m]]--;
-            if(ms[s[i-m]]==0){
-                ms.erase(s[i-m]);
+            if(match==freq.size()){
+                ans.push_back(start);
             }
-        }
-        if (ms == mp){
-            ans.push_back(s.length()-m);
+            if(end>=m-1){
+                if(freq.find(s[start])!=freq.end()){
+                    if(freq[s[start]]==0){
+                        match--;
+                    }
+                    freq[s[start]]++;
+                }
+                start++;
+            }
         }
         return ans;
     }
