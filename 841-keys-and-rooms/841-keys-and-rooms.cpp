97cce163233926ed4a25b15af3558b3496ece2a9
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2022-07-17 23:25:18 +0530

    Time: 826 ms (5.03%), Space: 152.3 MB (5.02%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    void dfsTravel(vector<vector<int>> rooms,vector<bool> &key,vector<bool> &visit,int node){
+    void dfsTravel(vector<vector<int>> rooms,vector<bool> &visit,int node){
         visit[node]=true;
         for(auto it:rooms[node]){
             if(!visit[it]){
-                key[it]=true;
-                dfsTravel(rooms,key,visit,it);
+                dfsTravel(rooms,visit,it);
             }
         }
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n=rooms.size();
-        vector<bool> key(n,false);
         vector<bool> visit(n,false);
-        key[0]=true;
+        dfsTravel(rooms,visit,0);
         for(int i=0;i<n;i++){
-            if(key[i] && !visit[i]){
-                dfsTravel(rooms,key,visit,i);
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(!key[i]){
+            if(!visit[i]){
                 return false;
             }
         }
