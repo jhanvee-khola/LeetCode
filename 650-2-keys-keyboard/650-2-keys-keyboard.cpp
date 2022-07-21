30b454e37b288b31066907b654dcd955cb753195
Jhanvee Khola <77978729+jhanvee-khola@users.noreply.github.com>
2022-07-21 16:12:52 +0530

    Time: 398 ms (5.08%), Space: 34.4 MB (15.62%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     set<pair<int,int>> visit;
+    int dp[1001][1001];
     int solve(int i,int j,int n){
         if(i>n){
             return 1e9;
@@ -11,16 +12,20 @@ public:
         if(visit.find({i,j})!=visit.end()){
             return 1e9;
         }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
         else{
             visit.insert({i,j});
         }
         int op1=1+solve(i,i,n);
         int op2=1+solve(i+j,j,n);
-        return min(op1,op2);
+        return dp[i][j]=min(op1,op2);
     }
     int minSteps(int n) {
         //i->no. of A on the screen
         //j->no. of A on the clipboard
-         return solve(1,0,n);
+        memset(dp,-1,sizeof(dp));
+        return solve(1,0,n);
     }
 };
